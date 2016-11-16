72b83e2042a393d5feecb93c9d0cada24fa41f50
Flake22 <fede.a@live.it>
2016-11-16 15:20:03 +0100

    Minor fixes

--- isochrone/views.py
+++ isochrone/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 import isochrone_computation
 import key_getter
+import os
 
 def index(request):
 	key = key_getter.get_key()
 	if request.method == "GET":
 		try:
+			#checks whether isochrone has been requested
 			latitude = request.GET["latitude"]
 			longitude = request.GET["longitude"]
 			duration = request.GET["duration"]
@@ -14,14 +16,29 @@ def index(request):
 			angles = request.GET["angles"]
 			tolerance = request.GET["tolerance"]
 
+			print "HELLO\n\n\n"
+
+			#get isochrone and its html versrion
 			iso, htmltext = isochrone_computation.compute_isochrone(latitude, longitude, duration, travelMode, angles, tolerance)
+			
 			#call postgress
+
 
 			print htmltext
 			print iso
 
-			return HttpResponse("Hello world")
-			#return render_to_response('isochrone/index.html', {'key' : key, 'map': "map.html", 'main': "main.html"})
+			try:
+				script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+				rel_path = "templates/iso_map.html"
+				abs_file_path = os.path.join(script_dir, rel_path)
+				with open(abs_file_path, 'w+') as f:
+					f.write(htmltext)
+			except Exception as e:
+				print e
+
+
+			#return HttpResponse("Hello world")
+			return render_to_response('isochrone/index.html', {'key' : key, 'map': "iso_map.html", 'main': "main.html", 'menu': "menu.html"})
 		except Exception:
 			pass
 	return render_to_response('isochrone/index.html', {'key' : key, 'map': "map.html", 'main': "main.html", 'menu': "menu.html"})
