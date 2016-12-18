7f82a2d5f9592d14325a917f5126bc7744daa926
Flake22 <fede.a@live.it>
2016-12-18 18:14:06 +0100

    added db interconnection

--- isochrone/db_connection.py
+++ isochrone/db_connection.py
@@ -1,7 +1,55 @@
 import psycopg2
+import pyproj
 
-def create_query():
-	pass
+def convert_points_to_UTM32N(iso):
+	# Define coordinates stadards
+	wgs84=pyproj.Proj("+init=EPSG:4326") # LatLon with WGS84 datum used by GPS units and Google Earth
+	UTM32N=pyproj.Proj("+init=EPSG:32632") # LatLon used by the database
+
+	s = ''
+
+	# Current coordinates that need to be converted (Trento)
+	# Remember to reverse the logic of lat-long in x-y cartesian system (lat becomes y and long becomes x)
+	for points in l:
+		y_curr = points[0]
+		x_curr = points[1]
+
+		# Conversion of the coordinates from wgs84 to UTM32N reference
+		x_tar, y_tar = pyproj.transform(wgs84, UTM32N, x_curr, y_curr)
+
+		s = ''.join([s, str(x_tar), ' ', str(y_tar), ','])
+
+	return s[:len(s)-1]
+
+def create_query(iso):
+	#convert WGS84 coordinates into UTM32N
+	polygon = convert_points_to_UTM32N(iso)
+
+	query = "\n.".join(["SELECT ",
+						"SUM(ST_Area(ST_GeomFromText('POLYGON(("+polygon+"))')))/SUM(ST_Area(geom)),",
+						"SUM(p.p1)+SUM(s.st1),",
+						"SUM(p.p2)+SUM(s.st2),",
+						"SUM(p.p14)+SUM(p.p15)+SUM(p.p16)+SUM(p.p17)+SUM(p.p18)+SUM(p.p19)+SUM(s.st3),",
+						"SUM(p.p20)+SUM(p.p21)+SUM(p.p22)+SUM(p.p23)+SUM(p.p24)+SUM(s.st4),",
+						"SUM(p.p25)+SUM(p.p26)+SUM(p.p27)+SUM(p.p28)+SUM(p.p29)+SUM(s.st5),",
+						"SUM(p.p30)+SUM(p.p31)+SUM(p.p32)+SUM(p.p33)+SUM(p.p34)+SUM(p.p35)+SUM(s.st6),",
+						"SUM(p.p36)+SUM(p.p37)+SUM(p.p38)+SUM(p.p39)+SUM(p.p40)+SUM(s.st7),",
+						"SUM(p.p41)+SUM(p.p42)+SUM(p.p43)+SUM(p.p44)+SUM(p.p45)+SUM(s.st8),",
+						"SUM(f.pf1),",
+						"SUM(f.pf3),",
+						"SUM(f.pf4),",
+						"SUM(f.pf5),",
+						"SUM(f.pf6),",
+						"SUM(f.pf7),",
+						"SUM(f.pf8),",
+						"SUM(p.p137),",
+						"SUM(p.p138)",
+						"FROM spatial_ref NATURAL JOIN ",
+						"famiglie AS f NATURAL JOIN ",
+						"popolazione_residente AS p NATURAL JOIN ",
+						"stranieri_residenti AS s WHERE ST_Intersects(geom, ST_GeomFromText('POLYGON(("+polygon+"))'))"])
+
+	return query
 
 def connect():
 	connection = psycopg2.connect("dbname='spatial' user='postgres' host='localhost' password='password'")
@@ -12,7 +60,7 @@ def executeQuery(iso, connection):
 	cursor = connection.cursor()
 	cursor.execute(query)
 	records = cursor.fetchall()
-	return records
+	return records[0]
 
 def disconnect(connection):
 	connection.close()
--- isochrone/views.py
+++ isochrone/views.py
@@ -6,6 +6,37 @@ import isochrone_computation
 import key_getter
 import db_connection
 
+def parse_query_results(res):
+	#get data for computations
+	percentage = float(res[0])
+
+	#retrive population by age and sex
+	m1 =int(round(res[6]*percentage))						#men 0-29
+	m2 =int(round(res[7]*percentage))						#men 30-54
+	m3 =int(round(res[8]*percentage))						#men >=55
+	men = m1+m2+m3
+
+	w1 =int(round(res[3]*percentage))-m1					#women 0-29
+	w2 =int(round(res[4]*percentage))-m2					#women 30-54
+	w3 =int(round(res[5]*percentage))-m3					#women >=55
+	women = (w1+w2+w3)
+
+	age_and_sex = [m1, m2, m2, w1, w2, w3, women, men]
+
+	#retrive family composition
+	family_composition=[int(round(res[10]*percentage)),		#families with 1 component
+						int(round(res[11]*percentage)),		#families with 1 component
+						int(round(res[12]*percentage)),		#families with 1 component
+						int(round(res[13]*percentage)),		#families with 1 component
+						int(round(res[14]*percentage)),		#families with 1 component
+						int(round(res[15]*percentage))]		#families with 1 component
+
+	#retrive commuters
+	commuters=[int(round(res[16]*percentage)),				#within residence town
+						int(round(res[17]*percentage))]		#outside residence town
+
+	print age_and_sex, family_composition, commuters
+
 def index(request):
 	key = key_getter.get_key()
 	if request.method == "GET":
@@ -18,37 +49,32 @@ def index(request):
 			angles = request.GET["angles"]
 			tolerance = request.GET["tolerance"]
 
-			print "HELLO\n\n\n"
-
 			#get isochrone and its html versrion
 			iso, htmltext = isochrone_computation.compute_isochrone(latitude, longitude, duration, travelMode, angles, tolerance)
 
-			print iso
+			#print htmltext to file	
+			try:
+				script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+				rel_path = "templates/iso_map.html"
+				abs_file_path = os.path.join(script_dir, rel_path)
+				with open(abs_file_path, 'w+') as f:
+					f.write(htmltext)
+			except Exception as e:
+				print e
+				#return HttpResponse("Hello world")
 			
 			#open connection with pgadming ad run query
 			# try:
+			#	#connect to the db
 			# 	conn = db_connection.connect()
-			# 	records = db_connection.executeQuery(iso, connection)
+			# 	records = db_connection.executeQuery(iso, conn)
 
-			# 	#do something with records
+			# 	age_and_sex, family_composition, commuters = parse_query_results(records)
 
 			# 	#close connection
 			# 	conn.close()
 			# except Exception as e:
 			# 	raise
-
-			# print htmltext
-			# print iso
-
-			# try:
-			# 	script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-			# 	rel_path = "templates/iso_map.html"
-			# 	abs_file_path = os.path.join(script_dir, rel_path)
-			# 	with open(abs_file_path, 'w+') as f:
-			# 		f.write(htmltext)
-			# except Exception as e:
-			# 	print e
-
 
 			#return HttpResponse("Hello world")
 			return render_to_response('isochrone/index.html', {'key' : key, 'map': "iso_map.html", 'main': "main.html", 'menu': "menu.html"})
