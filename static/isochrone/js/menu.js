ab662f995289d7e2685bb07785434e5eea4dd9e7
Flake22 <fede.a@live.it>
2016-12-19 18:10:03 +0100

    added bootstrap

---
+++ static/isochrone/js/menu.js
@@ -0,0 +1,45 @@
+
+$(document).ready(function(){
+	$('#buttonEnabled').click(function(){
+	  $('#buttonEnabled').hide();
+	  $('#buttonDisabled').show();
+	   }
+	   )
+	   
+	   
+	   $('#angles').slider({
+	formatter: function(value) {
+		return 'Current value: ' + value;
+	}
+	});
+	$("#angles").slider();
+		$("#angles").on("slide", function(slideEvt) {
+			$("#anglesSliderVal").text(slideEvt.value);
+		});
+
+	$('#tolerance').slider({
+		formatter: function(value) {
+			return 'Current value: ' + value;
+		}
+	});
+	$("#tolerance").slider();
+		$("#tolerance").on("slide", function(slideEvt) {
+			$("#toleranceSliderVal").text(slideEvt.value);
+		});
+
+	   
+		$('#ex1').slider({
+			formatter: function(value) {
+				return 'Current value: ' + value;
+			}
+		});
+		$("#slider").slider();
+			$("#slider").on("slide", function(slideEvt) {
+				$("#sliderSliderVal").text(slideEvt.value);
+			});
+	   
+	   });
+
+
+
+
