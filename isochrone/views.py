7ae78d68f28f9d3cb2bc7360e0ddf41529f2a891
SimoneScalco93 <simone.scalco@studenti.unitn.it>
2016-12-22 16:54:04 +0100

    GET parameters validation. Exception isolated

--- isochrone/views.py
+++ isochrone/views.py
@@ -47,6 +47,26 @@ def print_to_file(rel_path, html):
 def escape_html(html):
 	return "{% autoescape off %}{{" + myhtml + "}}{% endautoescape %}"
 
+
+def checkGetParams(latitude, longitude, duration, travelMode, angles, tolerance):
+	
+	validParams = True
+	if latitude == None or latitude == "":
+		validParams = False
+	elif longitude == None or longitude == "":
+		validParams = False
+	elif duration == None or duration == "":
+		validParams = False
+	elif travelMode == None or travelMode == "":
+		validParams = False
+	elif angles == None or travelMode == "":
+		validParams = False
+	elif tolerance == None or tolerance == "":
+		validParams = False
+
+	return validParams
+
+
 def index(request):
 	key = key_getter.get_key()
 	map_file = 'map.html'
@@ -54,48 +74,48 @@ def index(request):
 	if request.method == "GET":
 		try:
 			#checks whether isochrone has been requested
-			latitude = request.GET["latitude"]
-			longitude = request.GET["longitude"]
-			duration = request.GET["duration"]
-			travelMode = request.GET["travelMode"]
-			angles = request.GET["angles"]
-			tolerance = request.GET["tolerance"]
+			latitude = request.GET.get("latitude", None)
+			longitude = request.GET.get("longitude", None)
+			duration = request.GET.get("duration", None)
+			travelMode = request.GET.get("travelMode", None)
+			angles = request.GET.get("angles", None)
+			tolerance = request.GET.get("tolerance", None)
 
-			#get isochrone and its html versrion
-			iso, map_html = isochrone_computation.compute_isochrone(latitude, longitude, duration, travelMode, angles, tolerance)
-			
-			#iso = [[[46.07287],[11.12051]],[[46.08669],[11.14825]],[[46.06501],[11.15151]],[[46.07287],[11.12051]]]
-			#map_html = ""
+			if checkGetParams(latitude, longitude, duration, travelMode, angles, tolerance):
 
-
-			#open connection with pgadming ad run query
-			try:
-				#connect to the db
-			 	conn = db_connection.connect()
-			 	records = db_connection.executeQuery(iso, conn)
-
-			 	age_and_sex, family_composition, commuters = parse_query_results(records)
-
-			 	#close connection
-			 	conn.close()
-			except Exception as e:
-			 	print e
-			print "connection"
-			graphs_html = graphs_generator.generate_graphs(age_and_sex, family_composition, commuters)
-			print "done"
-
-			#print html to file	
-			try:
-				print_to_file("templates/iso_map.html", map_html)
-				print "iso_map"
-				print_to_file("templates/graphs.html", graphs_html)
-				print "graphs"
+				#get isochrone and its html versrion
+				iso, map_html = isochrone_computation.compute_isochrone(latitude, longitude, duration, travelMode, angles, tolerance)
 				
-				map_file = 'iso_map.html'
-				menu_file = 'graphs.html'
-			except Exception as e:
-				print e
+				#open connection with pgadming ad run query
+				try:
+					#connect to the db
+				 	conn = db_connection.connect()
+				 	records = db_connection.executeQuery(iso, conn)
 
-		except Exception:
-			pass
+				 	age_and_sex, family_composition, commuters = parse_query_results(records)
+
+				 	#close connection
+				 	conn.close()
+				except Exception as e:
+				 	print e
+				print "connection"
+				graphs_html = graphs_generator.generate_graphs(age_and_sex, family_composition, commuters)
+				print "done"
+
+				#print html to file	
+				try:
+					print_to_file("templates/iso_map.html", map_html)
+					print "iso_map"
+					print_to_file("templates/graphs.html", graphs_html)
+					print "graphs"
+					
+					map_file = 'iso_map.html'
+					menu_file = 'graphs.html'
+				except Exception as e:
+					print e
+
+		except Exception as e:
+			print e
+
+
 	return render_to_response('isochrone/index.html', {'key' : key, 'map': map_file, 'main': "main.html", 'menu': menu_file})
